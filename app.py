6878eba69c4737ac01c1bb0edbbb06d186fdc949
saadkiet <56345112+saadkiet@users.noreply.github.com>
2022-08-26 19:06:12 +0500

    Add additional Input box and download button
    Added an additional input box where use defines how many tweets the app has to fetch. Have also included download button to download dataframe and Wordcloud

--- app.py
+++ app.py
@@ -52,28 +52,31 @@ def app():
 
     elif choice=="Tweet Analyze":
         #st.subheader("Analyze the tweets of your favourite Personalities")
-        st.markdown("Here you get to perform sentiment analysis on tweets extracted from a Twitter user")
-        st.markdown("It performs 3 functions: ")
-        st.write("1. Displays the 5 most recent tweets from the given Twitter handler")
-        st.write("2. Generates a Word Cloud (***Most used words by the user***)")
-        st.write("3. Displays Sentiments of the last 200 tweets in the form of a bar plot")
+        st.markdown("Welcome to Twitter Sentiment Analyzer. Here you get to perform the following functions :")
+        st.write("1. Fetch recent Tweets from a particular user")
+        st.write("2. Generates  Word Cloud")
+        st.write("3. Displays Sentiments using a bar plot")
+        st.markdown("*Enter the user name and the number of tweets you wish to fetch in the fields below*")
 
         twitter_user = st.text_input("*Enter User ID of the Twitter handler without '@' in the empty field below*")
+        #limit_of_tweets = int(st.text_input("*How many recent Tweets you want to fetch?*"))
+        limit_of_tweets = st.text_input("*How many recent Tweets you want to fetch?*") # Passing the value as string. This value will be converted to Int below
         Analyzer_choice = st.selectbox("Select the Activities",["Show Recent Tweets", "Generate WordCloud", "Visualize the Sentiment Analysis"])
 
         if st.button("Analyze Tweet"):
             # SHow Recent Tweets
             if Analyzer_choice=='Show Recent Tweets':
-                st.success("Fetching the last 5 Tweets by the user, please wait...")
+                st.success("Fetching the last %d Tweets by the user, please wait..."%(int(limit_of_tweets)))
 
                 def show_recent_tweets(twitter_user):
-                    posts = api.user_timeline(screen_name=twitter_user, count=500, lang="en", tweet_mode="extended")
 
-                    def get_tweets():#
+                    posts = api.user_timeline(screen_name=twitter_user, count=limit_of_tweets, lang="en", tweet_mode="extended")
+
+                    def get_tweets():
                         print("Show the 5 recent tweets : \n")
                         l=[]
                         i=1
-                        for tweet in posts[0:5]:
+                        for tweet in posts[0:int(limit_of_tweets)]:
                             l.append(tweet.full_text)
                             i= i + 1
                         return l
@@ -84,12 +87,21 @@ def app():
 
             # Generating Wordcloud
             elif Analyzer_choice=='Generate WordCloud':
-                st.success("Generating Word Cloud of last 200 Tweets as well as Translating any Non-English Tweets to English, please wait...")
+                #st.success("Fetching the last %d Tweets by the user, please wait..."%(limit_of_tweets))
+                st.success("Generating Word Cloud of recent %d Tweets as well as Translating any Non-English Tweets to English, please wait..."%(int(limit_of_tweets)))
                 def get_wordcloud():
-                    posts = api.user_timeline(screen_name=twitter_user, count=500, lang="en", tweet_mode="extended")
+                    tweets = tweepy.Cursor(api.user_timeline, screen_name=twitter_user, tweet_mode='extended').items(int(limit_of_tweets))
+                    columns = ['Tweets']  # ['Time', 'User', 'Tweet']
+                    data = []
+                    for tweet in tweets:
+                        data.append([tweet.full_text])
+
+                    df = pd.DataFrame(data, columns=columns)
+
+                    #posts = api.user_timeline(screen_name=twitter_user, count=200, lang="en", tweet_mode="extended")
 
                     # Creating Dataframe of the tweets
-                    df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])
+                    #df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])
                     tweet_list = []
                     for i in range(len(df.Tweets)):
                         result = translator.translate(df.Tweets[i], dest="en")
@@ -125,25 +137,39 @@ def app():
                     return img
 
                 img = get_wordcloud()
-
                 st.image(img)
+
+                #You can convert your PIL Image to bytes using the following snippet
+                from io import BytesIO
+                buf = BytesIO()
+                img.save(buf, format="JPEG")
+                byte_im = buf.getvalue()
+
+                btn = st.download_button(
+                    label="Download Image",data=byte_im,file_name="imagename.png",mime="image/jpeg")
+
 
             else:
 
                 def plot_analysis():
-                    st.success("Generating visualization for Sentiment Analysis. Fetching last 200 tweets, converting Non-English Tweets to English, please wait...")
-                    posts = api.user_timeline(screen_name=twitter_user, count=500, lang="en", tweet_mode="extended")
+                    st.success("Generating visualization for Sentiment Analysis of recent %d tweets, also Translating Non-English Tweets to English, please wait..."%(int(limit_of_tweets)))
+                    #posts = api.user_timeline(screen_name=twitter_user, count=500, lang="en", tweet_mode="extended")
+                    #limit = 500
+                    tweets = tweepy.Cursor(api.user_timeline, screen_name=twitter_user, tweet_mode='extended').items(int(limit_of_tweets))
+                    columns = ['Tweets']  # ['Time', 'User', 'Tweet']
+                    data = []
+                    for tweet in tweets:
+                        data.append([tweet.full_text])
 
+                    df = pd.DataFrame(data, columns=columns)
                     # Creating Dataframe of the tweets
-                    df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])
+                    #df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])
+                    #Translating tweets
                     tweet_list = []
                     for i in range(len(df.Tweets)):
                         result = translator.translate(df.Tweets[i], dest="en")
                         tweet_list.append(result.text)
                     df1 = pd.DataFrame(tweet_list, columns=['Translated_Tweets'])
-
-                    # df1['clean_tweets'] = df1['Translated_Tweets']
-                    # df1['clean_tweets'] = df1['clean_tweets'].apply(preprocess)
 
                     def preprocess(text):
                         new_text = []
@@ -183,21 +209,37 @@ def app():
                 st.set_option('deprecation.showPyplotGlobalUse', False)
                 st.pyplot(use_container_width=True)
 
+                #You can convert your PIL Image to bytes using the following snippet
+                #from io import BytesIO
+                #buf = BytesIO()
+                #st.write(sns.countplot(x=df1["Analysis"], data=df1)).save(buf, format="JPEG")
+                #byte_im = buf.getvalue()
+
+                #btn = st.download_button(label="Download Image",data=st.pyplot(use_container_width=True),file_name="imagename.png",mime="image/jpeg")
+
 
 
     else:
-        st.subheader("Here we fetch the lastest 200 tweets and can perform the following tasks on them")
+        st.markdown("**Here we fetch the lastest tweets and can perform the following tasks on them**")
 
-        st.write("1. Fetches Tweets and makes a Dataframe for it")
-        st.write("2. Preprocesses and cleans the text")
-        st.write("3. Uses Roberta model to Analyze the sentiments of the Tweets")
+        st.write("1. Preprocess, Clean and Translate Tweets to English Language")
+        st.write("2. Analyze the Tweets using Roberta model and label the tweets as Positive, Negative or Neutral")
+        st.write("3. Display the data as a Dataframe")
         twitter_id = st.text_input("*Enter User ID of the Twitter handler without '@' in the empty field below*")
+        limit_of_tweets = st.text_input("*How many recent Tweets you want to fetch?*")
 
         def get_data(twitter_id):
-            posts = api.user_timeline(screen_name=twitter_id, count=100, lang="en", tweet_mode="extended")
-
-            df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])
+            #posts = api.user_timeline(screen_name=twitter_id, count=100, lang="en", tweet_mode="extended")
+            #df = pd.DataFrame([tweet.full_text for tweet in posts], columns=['Tweets'])
             #df['Translated_tweets']=df['Tweets']
+            tweets = tweepy.Cursor(api.user_timeline, screen_name=twitter_id, tweet_mode='extended').items(int(limit_of_tweets))
+            columns = ['Tweets']  # ['Time', 'User', 'Tweet']
+            data = []
+            for tweet in tweets:
+                data.append([tweet.full_text])
+
+            df = pd.DataFrame(data, columns=columns)
+
             tweet_list = []
             for i in range(len(df.Tweets)):
                 result = translator.translate(df.Tweets[i], dest="en")
@@ -233,9 +275,17 @@ def app():
 
             return df1
         if st.button('Show Data'):
-            st.success("Feteching last 200 tweets as well as converting Non_English Tweets to English, Please wait...")
+            st.success("Feteching recent %d tweets, Please wait..."%(int(limit_of_tweets)))
             df1=get_data(twitter_id)
             st.write(df1)
+
+            @st.cache
+            def convert_df(dataframe):
+                return dataframe.to_csv().encode('utf-8')
+            csv = convert_df(df1)
+            st.download_button(label="Download CSV",data=csv,file_name='output_df.csv',mime='text/csv')
+
+
 
 
 if __name__ == "__main__":
